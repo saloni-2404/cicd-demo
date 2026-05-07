ae547f9a88be84f8b7b4e1a2b87fac7a08c4dea3
Saloni <saloni.2404@gmail.com>
2026-05-07 19:01:24 +0530

    Fix the test

--- test_app.py
+++ test_app.py
@@ -1,5 +1,5 @@
 from app import hello
 
 def test_hello():
-    #assert hello() == "Hello from CI/CD"
-    assert hello() == "Wrong input"
+    assert hello() == "Hello from CI/CD"
+    #assert hello() == "Wrong input"
