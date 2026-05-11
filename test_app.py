3ca50bce4f7abe69d4061fe204651c205794bbeb
Saloni <saloni.2404@gmail.com>
2026-05-11 21:18:44 +0530

    Add a print command to the test

--- test_app.py
+++ test_app.py
@@ -2,4 +2,5 @@ from app import hello
 
 def test_hello():
     assert hello() == "Hello from CI/CD"
+    print("Test passed: hello() returned the correct message")
     #assert hello() == "Wrong input"
